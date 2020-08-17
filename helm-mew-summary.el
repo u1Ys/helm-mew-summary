581a29c1f79b5f18701a95243d62d24da7f7cca6
u1Ys <u1@berlb.net>
2020-08-18 00:34:47 +0900

    initial commit

---
+++ helm-mew-summary.el
@@ -0,0 +1,30 @@
+;; configure example
+;; 
+;; (require 'helm-mew-summary)
+;; (define-key mew-summary-mode-map (kbd "g")
+;;   'helm-mew-summary-goto-folder)
+;; (define-key mew-summary-mode-map (kbd "G")
+;;   'mew-summary-goto-folder)
+
+(require 'helm-config)
+
+(defun helm-mew-summary-goto-folder()
+  "helm interface for mew-summary-goto-folder (which is bind to 'g' in default)"
+  (interactive)
+  (helm :sources helm-mew-folder-source
+	:prompt "Folder Name: "
+	:preselect "%inbox"
+	))
+
+(defvar helm-mew-folder-source
+  '((name . "helm mew folder")
+    (candidates . (lambda ()
+		    (append
+		     (mew-local-folder-alist)
+		     (mapcar 'car (mew-imap-folder-alist)))
+		    ))
+    (action . mew-summary-visit-folder)
+    ))
+
+(provide 'helm-mew-summary)
+
