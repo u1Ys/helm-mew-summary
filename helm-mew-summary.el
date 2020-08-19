a95f75af9f8980e133ad8979f1ad7c8d46553e57
u1Ys <u1@berlb.net>
2020-08-19 22:07:33 +0900

    add refile interface

--- helm-mew-summary.el
+++ helm-mew-summary.el
@@ -8,6 +8,7 @@
 
 (require 'helm-config)
 
+;; ------------------------ goto folder
 (defun helm-mew-summary-goto-folder()
   "helm interface for mew-summary-goto-folder (which is bind to 'g' in default)"
   (interactive)
@@ -21,12 +22,43 @@
     (candidates . (lambda ()
 		    (append
 		     (mew-local-folder-alist)
-		     (mapcar 'car (mew-imap-folder-alist
-				   (if (mew-case-default-p mew-case)
-				       "" mew-case)))
+		     (mapcar 'car (helm-mew-imap-current-folder-alist))
 		     )))
     (action . mew-summary-visit-folder)
     ))
 
-(provide 'helm-mew-summary)
+;; ------------------------ refile
+(defvar use-helm-to-mew-refile 't)
+(if use-helm-to-mew-refile
+    (defun mew-input-refile-folders (folder-list singlep case proto)
+      (helm-mew-input-refile-folders folder-list)
+      ))
 
+(defun helm-mew-input-refile-folders (folder-list)
+  (interactive)
+  (helm :sources helm-mew-refile-source
+	:prompt "Folder Name: "
+	:preselect (car folder-list)
+	))
+
+(defvar helm-mew-refile-source
+      '((name . "helm refile folder")
+	(candidates . (lambda ()
+			(mapcar 'car (helm-mew-imap-current-folder-alist))
+			))
+	(action . (lambda (folder)
+		    ;; extract from original
+		    ;; "mew-input-refile-folders" function
+		    (mew-input-refile-folder-check
+		     (mapcar 'mew-chop (mew-split folder ?,))
+		     'imap 'local)
+		    ))
+	))
+
+;; ------------------------ utility function
+(defun helm-mew-imap-current-folder-alist ()
+  (mew-imap-folder-alist
+   (if (mew-case-default-p mew-case)
+       "" mew-case)))
+
+(provide 'helm-mew-summary)
