9edbc45c5de6c6db527d36a6a81052f84f1cde98
u1Ys <u1@berlb.net>
2020-09-06 15:09:05 +0900

    Create a folder when the folder specified by refill does not exist Changes to be committed: 	modified:   helm-mew-summary.el - change configure setting - add document

--- helm-mew-summary.el
+++ helm-mew-summary.el
@@ -3,14 +3,15 @@
 ;; (require 'helm-mew-summary)
 ;; (define-key mew-summary-mode-map (kbd "g")
 ;;   'helm-mew-summary-goto-folder)
-;; (define-key mew-summary-mode-map (kbd "G")
-;;   'mew-summary-goto-folder)
+;;
+;; (use-helm-to-mew-refile)
 
 (require 'helm-config)
 
 ;; ------------------------ goto folder
 (defun helm-mew-summary-goto-folder()
-  "helm interface for mew-summary-goto-folder (which is bind to 'g' in default)"
+  "helm interface for mew-summary-goto-folder (which is bind to
+'g' in default)"
   (interactive)
   (helm :sources helm-mew-folder-source
 	:prompt "Folder Name: "
@@ -28,32 +29,52 @@
     ))
 
 ;; ------------------------ refile
-(defvar use-helm-to-mew-refile 't)
-(if use-helm-to-mew-refile
-    (defun mew-input-refile-folders (folder-list singlep case proto)
-      (helm-mew-input-refile-folders folder-list)
-      ))
+(defun use-helm-to-mew-refile ()
+  "overwride `mew-input-refile-folders', that use on
+  `mew-summary-refile' (binded to `o' on default)->
+  `mew-summary-refile-body' -> `mew-refile-decide-folders' ->
+  `mew-input-refile-folders'"
+  (defun mew-input-refile-folders (folder-list singlep case proto)
+    (helm-mew-input-refile-folders folder-list)
+    ))
 
 (defun helm-mew-input-refile-folders (folder-list)
   (interactive)
-  (helm :sources helm-mew-refile-source
-	:prompt "Folder Name: "
-	:preselect (car folder-list)
-	))
+  (let 
+      ((default-select (car folder-list)))
+    (helm
+     :sources '(helm-mew-refile-source helm-mew-refile-fallback-source)
+     :input (mew-folder-prefix default-select)
+     :prompt "Folder Name: "
+     :preselect default-select
+     )))
 
 (defvar helm-mew-refile-source
-      '((name . "helm refile folder")
-	(candidates . (lambda ()
-			(mapcar 'car (helm-mew-imap-current-folder-alist))
-			))
-	(action . (lambda (folder)
-		    ;; extract from original
-		    ;; "mew-input-refile-folders" function
-		    (mew-input-refile-folder-check
-		     (mapcar 'mew-chop (mew-split folder ?,))
-		     'imap)
+  '((name . "Refile Folder")
+    (candidates . (lambda ()
+		    (mapcar 'car (helm-mew-imap-current-folder-alist))
 		    ))
-	))
+    (action . (lambda (folder)
+		(helm-mew-refile-action folder))
+	    )))
+
+(defvar helm-mew-refile-fallback-source
+  (helm-build-dummy-source
+      "Create Folder"
+    :action 'helm-mew-refile-action
+    :name "fallback"
+    )
+  "Even if enter an input that is not in candidates, do the same
+   action (creates a folder in `mew-refile-folder-check' function
+   in `mew-refile-decide-folders')"
+  )
+
+(defun helm-mew-refile-action (folder)
+  ;; extract from original `mew-input-refile-folders' function
+  (mew-input-refile-folder-check
+   (mapcar 'mew-chop (mew-split folder ?,))
+   (if (mew-folder-imapp proto) 'imap 'local)
+   ))
 
 ;; ------------------------ utility function
 (defun helm-mew-imap-current-folder-alist ()
