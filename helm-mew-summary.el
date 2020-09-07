eea868830ae2e511a461893dd08553134b6048c1
u1Ys <u1@berlb.net>
2020-09-07 21:41:08 +0900

    Remove "%" from intial input of refile

--- helm-mew-summary.el
+++ helm-mew-summary.el
@@ -11,7 +11,7 @@
 ;; ------------------------ goto folder
 (defun helm-mew-summary-goto-folder()
   "helm interface for mew-summary-goto-folder (which is bind to
-'g' in default)"
+  `g' in default)"
   (interactive)
   (helm :sources helm-mew-folder-source
 	:prompt "Folder Name: "
@@ -19,7 +19,7 @@
 	))
 
 (defvar helm-mew-folder-source
-  '((name . "helm mew folder")
+  '((name . "Folder")
     (candidates . (lambda ()
 		    (append
 		     (mew-local-folder-alist)
@@ -40,14 +40,11 @@
 
 (defun helm-mew-input-refile-folders (folder-list)
   (interactive)
-  (let 
-      ((default-select (car folder-list)))
     (helm
      :sources '(helm-mew-refile-source helm-mew-refile-fallback-source)
-     :input (mew-folder-prefix default-select)
      :prompt "Folder Name: "
-     :preselect default-select
-     )))
+     :preselect (car folder-list)
+     ))
 
 (defvar helm-mew-refile-source
   '((name . "Refile Folder")
@@ -70,11 +67,20 @@
   )
 
 (defun helm-mew-refile-action (folder)
-  ;; extract from original `mew-input-refile-folders' function
-  (mew-input-refile-folder-check
-   (mapcar 'mew-chop (mew-split folder ?,))
-   (if (mew-folder-imapp proto) 'imap 'local)
-   ))
+  (let*
+      ;; FIXME: `proto' is not function-local defined
+      ((proto-is-imap (mew-folder-imapp proto))
+       ;; add '%' to head if protocol is imap and
+       ;; `folder' is not start with '%'
+       (folder
+	(if (and proto-is-imap (not (mew-folder-imapp folder)))
+	    (concat "%" folder)
+       	  folder)))
+    ;; extract from original `mew-input-refile-folders' function
+    (mew-input-refile-folder-check
+     (mapcar 'mew-chop (mew-split folder ?,))
+     (if proto-is-imap 'imap 'local)
+     )))
 
 ;; ------------------------ utility function
 (defun helm-mew-imap-current-folder-alist ()
